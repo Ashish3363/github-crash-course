4b40113579887188bca65564524d26c22f1e9ba1
Ashish Kumar <ashish.kumar@artpark.com>
2026-04-26 11:15:24 +0530

    new add 4 number feature added

--- sum.java
+++ sum.java
@@ -7,6 +7,10 @@ public class sum {
         return (a + b + c);
     }
 
+    public static int addfournumber(int a, int b, int c, int d) {
+        return (a + b + c + d);
+    }
+
     public static void main(String[] args) {
         System.out.println("My name is Ashish");
         System.out.println("Ashish Kumar");
