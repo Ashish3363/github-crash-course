57c53b9feac13f5219738ee252e7a9fdb26befd1
Ashish Kumar <ashish.kumar@artpark.com>
2026-04-26 10:21:00 +0530

    new changes

--- diff.java
+++ diff.java
@@ -1,5 +1,7 @@
 public class diff {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("Bye");
     }
 }
--- sum.java
+++ sum.java
@@ -5,5 +5,6 @@ public class sum {
 
     public static void main(String[] args) {
         System.out.println("My name is Ashish");
+        System.out.println("Ashish Kumar");
     }
 }
