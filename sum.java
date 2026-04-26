dc9d224ba918b8e675d39b2d87b35e33624d3427
Ashish Kumar <ashish.kumar@artpark.com>
2026-04-26 09:56:00 +0530

    removed files

--- sum.java
+++ sum.java
@@ -1,6 +1,9 @@
 public class sum {
+    public static int add(int a, int b) {
+        return (a + b);
+    }
+
     public static void main(String[] args) {
         System.out.println("My name is Ashish");
-        System.out.println("Kumar");
     }
 }
